29253d073db2d32b317b2cb0fef530fe29f30462
Paul Sho <Cyberkiller.CK@gmail.com>
2023-12-18 14:29:04 +0800

    added quiz pages
    Co-Authored-By: Dexter Magz <116888584+DexterMaghanoy@users.noreply.github.com>

---
+++ teacher/add_quiz.php
@@ -0,0 +1,122 @@
+<?php
+session_start();
+include '../includes/connection.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if (isset($_SESSION['teacher_id'])) {
+    $teacher_id = $_SESSION['teacher_id'];
+} else {
+    $teacher_id = '';
+    header('location:index.php');
+}
+
+if (isset($_POST['submit'])) {
+    $status = $_POST['status'];
+    $status = filter_var($status, FILTER_SANITIZE_STRING);
+    $title = $_POST['title'];
+    $title = filter_var($title, FILTER_SANITIZE_STRING);
+    $description = $_POST['description'];
+    $description = filter_var($description, FILTER_SANITIZE_STRING);
+    $lessons = $_POST['lessons'];
+    $lessons = filter_var($lessons, FILTER_SANITIZE_STRING);
+    $quiz_type = $_POST['quiz_type'];
+    $quiz_type = filter_var($quiz_type, FILTER_SANITIZE_STRING);
+    $retryAfter = $_POST['retryAfter'];
+    $retryAfterSeconds = $retryAfter * 3600;
+
+    $add_quiz = $conn->prepare("INSERT INTO `tbl_quiz` (`lesson_id`, `quiz_title`, `quiz_description`, `quiz_type`, `retryAfter`, `quiz_created`, `status`) VALUES (?, ?, ?, ?, ?, NOW(), ?)");
+    $add_quiz->execute([$lessons, $title, $description, $quiz_type, $retryAfterSeconds, $status]);
+
+    $message[] = 'New quiz added!';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="styles/main_style.css">
+
+</head>
+
+<body>
+
+    <?php include '../includes/teacher_header.php'; ?>
+
+    <section class="video-form">
+
+        <h1 class="heading">upload content</h1>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <p>Quiz status <span>*</span></p>
+            <select name="status" class="box" required>
+                <option value="" selected disabled>-- select status</option>
+                <option value="active">active</option>
+                <option value="deactive">inactive</option>
+            </select>
+            <p>Quiz title <span>*</span></p>
+            <input type="text" name="title" maxlength="100" required placeholder="enter Quiz title" class="box">
+            <p>Quiz description <span>*</span></p>
+            <textarea name="description" class="box" required placeholder="write description" maxlength="1000" cols="30"
+                Quiz="10"></textarea>
+            <p>Quiz lesson <span>*</span></p>
+            <select name="lessons" class="box" required>
+                <option value="" disabled selected>--select lesson</option>
+                <?php
+                $select_lessons = $conn->prepare("SELECT * FROM `tbl_lessons` WHERE teacher_id = ?");
+                $select_lessons->execute([$teacher_id]);
+                if ($select_lessons->rowCount() > 0) {
+                    while ($fetch_lessons = $select_lessons->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                        <option value="<?= $fetch_lessons['lesson_id']; ?>">
+                            <?= $fetch_lessons['lesson_title']; ?>
+                        </option>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                } else {
+                    echo '<option value="" disabled>no material created yet!</option>';
+                }
+                ?>
+            </select>
+            <p>Quiz Type <span>*</span></p>
+            <select name="quiz_type" class="box" required>
+                <option value="" disabled selected>--Select Quiz Type</option>
+                <option value="pre-test">Pre-Test</option>
+                <option value="post-test">Post-Test</option>
+            </select>
+            <p>If post-test failed, Retry After (in Hour): <span>*</span></p>
+
+            <input type="text" name="retryAfter" id="retryAfter" required placeholder="Enter Retry Timer in Hour" class="box">
+            <input type="submit" value="Create Quiz" name="submit" class="btn">
+
+        </form>
+
+    </section>
+    <script>
+        const input = document.getElementById('retryAfter');
+        const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        input.addEventListener('keyup', function (event) {
+            if (numbers.indexOf(event.key) == -1) {
+                input.value = input.value.replace(/[^0-9]/g, '');
+            } 
+        });
+    </script>
+    <script src="../scripts/script.js"></script>
+
+
+</body>
+
+</html>
---
+++ teacher/quizzes.php
@@ -0,0 +1,165 @@
+<?php
+session_start();
+include '../includes/connection.php';
+
+if (isset($_SESSION['teacher_id'])) {
+    $teacher_id = $_SESSION['teacher_id'];
+} else {
+    $teacher_id = '';
+    header('location:index.php');
+}
+
+
+// if (isset($_POST['delete_material'])) {
+//     $delete_id = $_POST['material_id'];
+//     $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+//     $verify_material = $conn->prepare("SELECT * FROM `tbl_learningmaterials` WHERE material_id = ? LIMIT 1");
+//     $verify_material->execute([$delete_id]);
+//     if ($verify_material->rowCount() > 0) {
+//         $delete_material_thumb = $conn->prepare("SELECT * FROM `tbl_learningmaterials` WHERE material_id = ? LIMIT 1");
+//         $delete_material_thumb->execute([$delete_id]);
+//         $fetch_thumb = $delete_material_thumb->fetch(PDO::FETCH_ASSOC);
+//         unlink('../tmp/' . $fetch_thumb['thumbnail']);
+//         $delete_material = $conn->prepare("SELECT * FROM `tbl_learningmaterials` WHERE material_id = ? LIMIT 1");
+//         $delete_material->execute([$delete_id]);
+//         $fetch_material = $delete_material->fetch(PDO::FETCH_ASSOC);
+//         unlink('../tmp/' . $fetch_material['file']);
+//         $delete_comments = $conn->prepare("DELETE FROM `tbl_comments` WHERE material_id = ?");
+//         $delete_comments->execute([$delete_id]);
+//         $delete_content = $conn->prepare("DELETE FROM `tbl_learningmaterials` WHERE material_id = ?");
+//         $delete_content->execute([$delete_id]);
+//         $message[] = 'material deleted!';
+//     } else {
+//         $message[] = 'material already deleted!';
+//     }
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="styles/main_style.css">
+
+</head>
+
+<body>
+
+    <?php include '../includes/teacher_header.php'; ?>
+
+    <section class="contents">
+
+        <h1 class="heading">your materials</h1>
+
+        <div class="box-container">
+
+            <div class="box" style="text-align: center;">
+                <h3 class="title" style="margin-bottom: .5rem;">create new quiz</h3>
+                <a href="add_quiz.php" class="btn">add quiz</a>
+            </div>
+
+            <?php
+            $itemsPerPageMaterials = 3;
+            $pageMaterials = isset($_GET['pageMaterials']) ? $_GET['pageMaterials'] : 1;
+            $offsetMaterials = ($pageMaterials - 1) * $itemsPerPageMaterials;
+
+            $countQuery = $conn->prepare("SELECT COUNT(*) FROM tbl_quiz q
+                            INNER JOIN tbl_lessons l ON q.lesson_id = l.lesson_id
+                            WHERE l.teacher_id = ?");
+            $countQuery->execute([$teacher_id]);
+            $totalItemsMaterials = $countQuery->fetchColumn();
+
+            $select_quiz = $conn->prepare("SELECT q.*, l.teacher_id 
+            FROM tbl_quiz q
+            INNER JOIN tbl_lessons l ON q.lesson_id = l.lesson_id
+            WHERE l.teacher_id = ? 
+            ORDER BY q.quiz_created DESC
+            LIMIT $offsetMaterials, $itemsPerPageMaterials");
+            $select_quiz->execute([$teacher_id]);
+
+            if ($select_quiz->rowCount() > 0) {
+                while ($fetch_materials = $select_quiz->fetch(PDO::FETCH_ASSOC)) {
+                    $material_id = $fetch_materials['material_id'];
+                    ?>
+                    <div class="box">
+                        <div class="flex">
+                            <div>
+                                <i class="fas fa-dot-circle" style="<?php
+                                if ($fetch_materials['status'] == 'active') {
+                                    echo 'color:limegreen';
+                                } else {
+                                    echo 'color:red';
+                                } ?>"></i>
+                                <span style="<?php
+                                if ($fetch_materials['status'] == 'active') {
+                                    echo 'color:limegreen';
+                                } else {
+                                    echo 'color:red';
+                                } ?>">
+                                    <?= $fetch_materials['status']; ?>
+                                </span>
+                            </div>
+                            <div>
+                                <i class="fas fa-calendar"></i>
+                                <span>
+                                    <?= $fetch_materials['quiz_created']; ?>
+                                </span>
+                            </div>
+                        </div>
+                        <img src="../tmp/<?= $fetch_materials['thumbnail']; ?>" class="thumb" alt="">
+                        <h3 class="title">
+                            <?= $fetch_materials['material_title']; ?>
+                        </h3>
+                        <form action="" method="post" class="flex-btn">
+                            <input type="hidden" name="material_id" value="<?= $material_id; ?>">
+                            <a href="update_material.php?get_id=<?= $material_id; ?>" class="option-btn">update</a>
+                            <input type="submit" value="delete" class="delete-btn"
+                                onclick="return confirm('delete this material?');" name="delete_material">
+                        </form>
+                        <a href="view_material.php?get_id=<?= $material_id; ?>" class="btn">view content</a>
+                    </div>
+                    <?php
+                }
+            } else {
+                echo '<p class="empty">no contents added yet!</p>';
+            }
+            ?>
+
+
+        </div>
+
+    </section>
+
+    <?php
+    $totalPagesMaterials = ceil($totalItemsMaterials / $itemsPerPageMaterials);
+
+    echo '<div class="pagination">';
+    if ($pageMaterials > 1) {
+        echo '<a href="?pageMaterials=' . ($pageMaterials - 1) . '">Previous</a>';
+    }
+    for ($i = 1; $i <= $totalPagesMaterials; $i++) {
+        echo '<a href="?pageMaterials=' . $i . '" class="' . ($pageMaterials == $i ? 'active' : '') . '">' . $i . '</a>';
+    }
+    if ($pageMaterials < $totalPagesMaterials) {
+        echo '<a href="?pageMaterials=' . ($pageMaterials + 1) . '">Next</a>';
+    }
+    echo '</div>';
+    ?>
+
+
+
+    <script src="../scripts/script.js"></script>
+
+</body>
+
+</html>
