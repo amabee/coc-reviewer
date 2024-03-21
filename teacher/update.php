330632b3b7ed0c77b89f7560ae0c7b1917b4d5fd
Paul Sho <Cyberkiller.CK@gmail.com>
2024-03-21 09:19:34 +0800

    update

--- teacher/update.php
+++ teacher/update.php
@@ -9,12 +9,11 @@ if (isset($_SESSION['teacher_id'])) {
     header('location:index.php');
 }
 
-if(isset($_SESSION["update_pass_alert"]) == true){
+if (isset($_SESSION["update_pass_alert"])) {
     $message[] = "Please update your default password to a new one";
 }
 
 if (isset($_POST['submit'])) {
-
     $select_teacher = $conn->prepare("SELECT * FROM `tbl_teachers` WHERE teacher_id = ? LIMIT 1");
     $select_teacher->execute([$teacher_id]);
     $fetch_teacher = $select_teacher->fetch(PDO::FETCH_ASSOC);
@@ -30,18 +29,18 @@ if (isset($_POST['submit'])) {
     if (!empty($name)) {
         $update_name = $conn->prepare("UPDATE `tbl_teachers` SET firstname = ? WHERE teacher_id = ?");
         $update_name->execute([$name, $teacher_id]);
-        $message[] = 'username updated successfully!';
+        $message[] = 'Username updated successfully!';
     }
 
     if (!empty($email)) {
         $select_email = $conn->prepare("SELECT email FROM `tbl_teachers` WHERE teacher_id = ? AND email = ?");
         $select_email->execute([$teacher_id, $email]);
         if ($select_email->rowCount() > 0) {
-            $message[] = 'email already taken!';
+            $message[] = 'Email already taken!';
         } else {
             $update_email = $conn->prepare("UPDATE `tbl_teachers` SET email = ? WHERE teacher_id = ?");
             $update_email->execute([$email, $teacher_id]);
-            $message[] = 'email updated successfully!';
+            $message[] = 'Email updated successfully!';
         }
     }
 
@@ -55,7 +54,7 @@ if (isset($_POST['submit'])) {
 
     if (!empty($image)) {
         if ($image_size > 2000000) {
-            $message[] = 'image size too large!';
+            $message[] = 'Image size too large!';
         } else {
             $update_image = $conn->prepare("UPDATE `tbl_teachers` SET `image` = ? WHERE teacher_id = ?");
             $update_image->execute([$rename, $teacher_id]);
@@ -63,7 +62,7 @@ if (isset($_POST['submit'])) {
             if ($prev_image != '' and $prev_image != $rename) {
                 unlink('../tmp/' . $prev_image);
             }
-            $message[] = 'image updated successfully!';
+            $message[] = 'Image updated successfully!';
         }
     }
 
@@ -75,22 +74,29 @@ if (isset($_POST['submit'])) {
     $cpass = sha1($_POST['cpass']);
     $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
 
-    if ($old_pass != $empty_pass) {
-        if ($old_pass != $prev_pass) {
-            $message[] = 'old password not matched!';
-        } elseif ($new_pass != $cpass) {
-            $message[] = 'confirm password not matched!';
-        } else {
-            if ($new_pass != $empty_pass) {
-                $update_pass = $conn->prepare("UPDATE `tbl_teachers` SET password = ? WHERE teacher_id = ?");
-                $update_pass->execute([$cpass, $teacher_id]);
-                $message[] = 'password updated successfully!';
+    // Password policy enforcement
+    $passwordPolicyMessage = enforce_password_policy($_POST['new_pass']);
+    if ($passwordPolicyMessage !== "") {
+        $message[] = $passwordPolicyMessage;
+    } else {
+        if ($old_pass != $empty_pass) {
+            if ($old_pass != $prev_pass) {
+                $message[] = 'Old password not matched!';
+            } elseif ($new_pass != $cpass) {
+                $message[] = 'Confirm password not matched!';
             } else {
-                $message[] = 'please enter a new password!';
+                if ($new_pass != $empty_pass) {
+                    $update_pass = $conn->prepare("UPDATE `tbl_teachers` SET password = ? WHERE teacher_id = ?");
+                    $update_pass->execute([$cpass, $teacher_id]);
+                    $message[] = 'Password updated successfully!';
+                } else {
+                    $message[] = 'Please enter a new password!';
+                }
             }
         }
     }
 }
+
 function random_string()
 {
     $str = random_bytes(16);
@@ -98,6 +104,37 @@ function random_string()
     $str = str_replace(["+", "/", "="], "", $str);
     $str = substr($str, 0, 16);
     return $str;
+}
+
+function enforce_password_policy($password)
+{
+    $minLength = 8; 
+    $requiresSpecialChar = true; 
+    $requiresNumber = true; 
+    $requiresAlpha = true; 
+
+    if (stripos($password, 'password') !== false) {
+        return "Choosen password is too weak, try a stronger one said goku";
+    }
+
+    if (strlen($password) < $minLength) {
+        return "Password must be at least $minLength characters long!";
+    }
+
+    if ($requiresSpecialChar && !preg_match('/[\'^£$%&*()}{@#~!?><>,|=_+¬-]/', $password)) {
+        return "Password must contain at least one special character!";
+    }
+
+
+    if ($requiresNumber && !preg_match('/[0-9]/', $password)) {
+        return "Password must contain at least one number!";
+    }
+
+    if ($requiresAlpha && !preg_match('/[a-zA-Z]/', $password)) {
+        return "Password must contain at least one alphabet character!";
+    }
+
+    return "";
 }
 
 ?>
@@ -128,7 +165,7 @@ function random_string()
     <section class="form-container" style="min-height: calc(100vh - 19rem);">
 
         <form class="register" action="" method="post" enctype="multipart/form-data">
-            <h3>update profile</h3>
+            <h3>Update Profile</h3>
             <div class="flex">
                 <div class="col">
                     <p>Your name </p>
@@ -138,25 +175,24 @@ function random_string()
                 </div>
                 <div class="col">
                     <p>Old password :</p>
-                    <input type="password" name="old_pass" placeholder="enter your old password" maxlength="20" class="box">
+                    <input type="password" name="old_pass" placeholder="Enter your old password" maxlength="20" minlength="10" class="box">
                     <p>New password :</p>
-                    <input type="password" name="new_pass" placeholder="enter your new password" maxlength="20" class="box">
+                    <input type="password" name="new_pass" placeholder="Enter your new password" maxlength="20" minlength="10" class="box">
                     <p>Confirm password :</p>
-                    <input type="password" name="cpass" placeholder="confirm your new password" maxlength="20" class="box">
+                    <input type="password" name="cpass" placeholder="Confirm your new password" maxlength="20" minlength="10" class="box">
                 </div>
             </div>
             <p>Update pic :</p>
             <input type="file" name="image" accept="image/*" class="box">
-            <input type="submit" name="submit" value="update now" class="btn">
+            <input type="submit" name="submit" value="Update Now" class="btn">
         </form>
 
     </section>
 
-    <!-- registe section ends -->
-
+    <!-- register section ends -->
 
     <script src="../scripts/script.js"></script>
 
 </body>
 
-</html
+</html>
